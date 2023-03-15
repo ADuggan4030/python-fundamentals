e533e8d1410160c90bf6793b62141b0fe7a223ec
April Duggan <aduggan403@gmail.com>
2023-03-14 22:41:08 -0500

    All About Numbers - Lab

---
+++ src/labs/numberslab.py
@@ -0,0 +1,39 @@
+# my second Python Lab Assignment
+# All About Numbers -- Tasks 1-2
+# Task 1
+
+# int with numeric seperator
+amount: int = 34_038
+# float with numeric seperator
+value: float = 45_838.8734
+# int with numeric seperator
+very_small_num: int = -3_695_365
+# int with numeric seperator
+Binary: int = 0b_0110_0010_1011
+# int with numeric seperator
+Hexadecimal: int = 0x_174C0
+
+# Task 2 - Value of 29
+""" Below functions represent Numeric types based
+on the number 29. 
+"""
+
+
+# hexadecimal
+def basic_hexadecimal():
+    print(0x1d)
+
+
+# binary
+def basic_binary():
+    print(0b11101)
+
+
+# octal
+def basic_octal():
+    print(0o35)
+
+
+basic_hexadecimal()
+basic_binary()
+basic_octal()

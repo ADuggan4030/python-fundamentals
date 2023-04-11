92d6a34678ae30559ea9bdfe297da3d0d6788a25
April Duggan <aduggan403@gmail.com>
2023-04-11 15:56:53 -0500

    myPracticeExceptions.py
    my own Practice for Exceptions

--- src/practice/myPracticeExceptions.py
+++ src/practice/myPracticeExceptions.py
@@ -1,7 +1,7 @@
 """ My Practice - Scratch Lab """
 
-
-def leap_year_demo(year: int):
+# commented out for other practice
+# def leap_year_demo(year: int):
     try:
         greeting = input('This will check if a year is a leap year.\nNow press Enter key.')
         year = int(input('Please input year in question.'))
@@ -16,6 +16,11 @@ def leap_year_demo(year: int):
             print(f'Your {year}, is not a leap year.')
 
 
-if __name__ == '__main__':
-    leap_year_demo(123)
+# if __name__ == '__main__':
+    # leap_year_demo(123)
 
+
+def leap_year_demo(year: int):
+    greeting = input('This will check if a year is a leap year.\nNow press Enter key.')
+    year = int(input('Please input year in question.'))
+    if year > 1900:
